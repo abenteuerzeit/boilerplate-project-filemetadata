0986d17a0de1c3e95089bac7bfbd513001d03621
Replit user <>
2023-10-29 11:54:59 +0100

    build(app): unzip replit solution

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+require('dotenv').config();
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: 'Please upload a file' });
+  }
+  const response = {
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  };
+  res.json(response);
+});
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+app.listen(port, () => {
+  console.log(`Your app is listening on port ${port}`);
 });
